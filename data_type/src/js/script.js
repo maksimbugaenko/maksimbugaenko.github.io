a07917467c3a899b057ecd46a86b95325ec77827
maksimbugaenko <maksimbugaenko88@gmail.com>
2019-04-16 19:07:29 +0300

    home

--- data_type/src/js/script.js
+++ data_type/src/js/script.js
@@ -1,26 +1,21 @@
 ;(function(){
 	"use strict";
-	
-
 	var login = prompt('Введите логин');
-	
+
 
 	if (login === 'Админ') {
-		var password = prompt ('Введите пароль')
-	}  else if (login === null) {
+		var password = prompt ('Введите пароль');
+		if (password === 'Чёрный Властелин') {
+			alert ('Добро пожаловать Чёрный Властелин ')
+		} else if (password === null) {
+			alert ('Вход отменён');
+		} else {
+			alert ('Пароль неверен');
+		}
+	} else if (login === null) {
 		alert ('Вход отменен');
-	} else if (password !== 'Чёрный Властелин') {
-		alert ('Пароль неверен');
 	} else {
 		alert ('Я вас не знаю');
 	}
 
-
-	if (password === 'Чёрный Властелин') {
-		alert ('Добро пожаловать Чёрный Властелин ')
-	} else if (password === null) {
-		alert ('Вход отменён');
-	}
-
-	
 })();
