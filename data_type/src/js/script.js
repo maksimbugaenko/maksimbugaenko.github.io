40bc5271a7b45b2122a044a31bd0b1408dff62a1
maksimbugaenko <maksimbugaenko88@gmail.com>
2019-04-16 17:42:51 +0300

    home

--- data_type/src/js/script.js
+++ data_type/src/js/script.js
@@ -1,12 +1,16 @@
 ;(function(){
 	"use strict";
+	
+
 	var login = prompt('Введите логин');
 	
 
 	if (login === 'Админ') {
 		var password = prompt ('Введите пароль')
-	} else if (login === null) {
+	}  else if (login === null) {
 		alert ('Вход отменен');
+	} else if (password !== 'Чёрный Властелин') {
+		alert ('Пароль неверен');
 	} else {
 		alert ('Я вас не знаю');
 	}
@@ -16,51 +20,7 @@
 		alert ('Добро пожаловать Чёрный Властелин ')
 	} else if (password === null) {
 		alert ('Вход отменён');
-	} else {
-		alert ('Пароль неверен');
-	}  
+	}
 
 	
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-	// if (login === 'Админ') {
-	// 	var password = prompt('Введите пароль');
-	// }
-	// if (password === 'Черный Властелин') {
-	// 	alert('Добро пожаловать!');
-	// } else{
-	// 	alert ("Пароль не верен");
-	// }
-
-	// if (login !== 'Админ')  {
-	// 	alert ("Я вас не знаю");
-	// }
-
-	// if (login === null) {
-	// 	alert ("Вход отменён");
-	// }
-	
-	// if (password === null) {
-	// 	alert ("Вход отменен");
-	// }
-	
-	// else if {
-	// 	alert ('Пароль не верен');
-	// }
-	// if (password !== 'Черный Властелин')  {
-	// // 	alert ("Пароль не верен");
-	// // }
 })();
---
+++ objects_arrays/src/js/script.js
@@ -0,0 +1,22 @@
+;(function IIFE(){
+	"use strict";
+	
+alert('Возведение элемнтов массива в куб.');
+
+	let arr=[6,2,4];
+
+	function getCube(x) {
+		return x*x*x;
+	}
+
+
+
+
+
+	// var number = prompt("Введите числа через запятую");
+	// var numbers = number.split(",");
+	// var n = prompt("Умножаем числа на ...");
+	// var multiply = alert (number * n);
+
+
+}) ();
